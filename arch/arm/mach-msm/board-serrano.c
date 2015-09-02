761b2e972d82e2d7f61d835a406262497a5a540b
MrKostoulhs@gmail.com <MrKostoulhs@gmail.com>
2015-09-02 19:53:11 +0300

    add intelli thermal
    Change-Id: Ifd72ca4cda19ca6f7136049ffc691f68debfcc32
    
    Conflicts:
    	arch/arm/configs/cyanogen_lt02ltexx_defconfig
    	arch/arm/mach-msm/board-loganre.c

--- arch/arm/mach-msm/board-serrano.c
+++ arch/arm/mach-msm/board-serrano.c
@@ -3479,10 +3479,29 @@ static struct platform_device msm_tsens_device = {
 
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 9,
+#ifdef CONFIG_INTELLI_THERMAL
 	.poll_ms = 250,
+#ifdef CONFIG_CPU_OVERCLOCK
 	.limit_temp_degC = 70,
+#else
+	.limit_temp_degC = 60,
+#endif
 	.temp_hysteresis_degC = 10,
-	.freq_step = 2,
+        .freq_step = 2,
+        .freq_control_mask = 0xf,
+        .core_limit_temp_degC = 80,
+	.core_temp_hysteresis_degC = 10,
+	.core_control_mask = 0xe,
+#else
+	.poll_ms = 1000,
+#ifdef CONFIG_CPU_OVERCLOCK
+	.limit_temp_degC = 70,
+#else
+	.limit_temp_degC = 60,
+#endif
+	.temp_hysteresis_degC = 10,
+        .freq_step = 2,
+#endif
 };
 
 #ifdef CONFIG_MSM_FAKE_BATTERY
