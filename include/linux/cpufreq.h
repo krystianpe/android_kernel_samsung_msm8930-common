1ea310356fd5de264105e59ed2f295365063c768
alexax66 <alexax66@gmail.com>
2015-11-18 19:15:52 +0000

    cpufreq: Add Alucard CPU Governor
    Alucard is based on Ondemand but has been heavily tweaked to bring better battery
    life and performance. It has been known to be battery friendly without sacrificing
    much performance.
    
    Based on commits:
     https://github.com/zakee94/halogen_reloaded/commit/744233e8ebf39f2b9f65ecbe58562edfb0829377
     https://github.com/zakee94/halogen_reloaded/commit/81a1a049321570520087008b68ef671a8e7ffc60

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -416,6 +416,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
 extern struct cpufreq_governor cpufreq_gov_smartmax_eüs;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #endif
 
 
