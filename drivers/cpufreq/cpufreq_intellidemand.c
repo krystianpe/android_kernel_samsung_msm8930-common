F4k/f4ktion_kernel

cpufreq_intellidemand.c: enable touch boost for S4 Mini touchscreen

drivers/cpufreq/cpufreq_intellidemand.c
@@ -1746,6 +1746,7 @@ static void dbs_input_event(struct input_handle *handle, unsigned int type,
 static int input_dev_filter(const char *input_dev_name)
 {
 	if (strstr(input_dev_name, "touchscreen") ||
+		strstr(input_dev_name, "mxts") ||
 		strstr(input_dev_name, "sec_touchscreen") ||
 		strstr(input_dev_name, "-keypad") ||
 		strstr(input_dev_name, "-nav") ||

krystianpe/android_kernel_samsung_msm8930-common

drivers/cpufreq/cpufreq_intellidemand.c
@@ -1746,6 +1746,7 @@ static void dbs_input_event(struct input_handle *handle, unsigned int type,
 static int input_dev_filter(const char *input_dev_name)
 {
 	if (strstr(input_dev_name, "touchscreen") ||
+		strstr(input_dev_name, "mxts_msm8930") ||
 		strstr(input_dev_name, "sec_touchscreen") ||
 		strstr(input_dev_name, "-keypad") ||
 		strstr(input_dev_name, "-nav") ||