89b2b526370431592b57923e61dd6ff4b00e1a19
Endre <ecseko@gmail.com>
2016-08-02 22:37:20 -0400

    set hard limit to 2052MHz

--- include/linux/cpufreq_hardlimit.h
+++ include/linux/cpufreq_hardlimit.h
@@ -19,7 +19,7 @@
 
 extern unsigned int cpufreq_hardlimit;
 
-#define CPUFREQ_HARDLIMIT_STOCK 1728000	/* default */
+#define CPUFREQ_HARDLIMIT_STOCK 2052000	/* default */
 
 unsigned int check_cpufreq_hardlimit(unsigned int freq);
 
